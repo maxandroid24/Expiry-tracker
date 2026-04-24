e8e64be3524cebb0aaaecf9f4443264ef1afec2e
Max <navneetkhar24@gmail.com>
2026-04-24 20:44:54 +0530

    build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,8 @@
 plugins {
     kotlin("jvm") version "1.5.30"
     application
+    id("com.android.application") version "8.2.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
 }
 
 application {
